24cd6aac8402b5f4a92d39f209e6cb5e5f5f0fd9
SakariKomscha <sakari.komscha@edu.turkuamk.fi>
2022-04-02 15:26:24 +0300

    HandGrab
    Hand grabbing ja snap to location on lisätty

---
+++ Blood Transfusion/Assets/SnapObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapObject : MonoBehaviour
+{
+    public GameObject SnapLocation;
+
+    public GameObject kiinnike;
+
+    public bool isSnapped;
+
+    private bool objectSnapped;
+
+    private bool grabbed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        grabbed = GetComponent<OVRGrabbable>().isGrabbed;
+
+        objectSnapped = SnapLocation.GetComponent<SnapToLocation>().Snapped;
+
+        if (objectSnapped == true)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.SetParent(kiinnike.transform);
+            isSnapped = true;
+        }
+
+        if (objectSnapped == false && grabbed == false)
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
---
+++ Blood Transfusion/Assets/SnapToLocation.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToLocation : MonoBehaviour
+{
+    private bool grabbed;
+
+    private bool insideSnapZone;
+
+    public bool Snapped;
+
+    public GameObject KiinnikeOsa;
+    public GameObject SnapRotationReference;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == KiinnikeOsa.name)
+        {
+            insideSnapZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == KiinnikeOsa.name)
+        {
+            insideSnapZone = false;
+        }
+    }
+
+    void SnapObject()
+    {
+        if (grabbed == false && insideSnapZone == true)
+        {
+            KiinnikeOsa.gameObject.transform.position = transform.position;
+            KiinnikeOsa.gameObject.transform.rotation = SnapRotationReference.transform.rotation;
+            Snapped = true;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        grabbed = KiinnikeOsa.GetComponent<OVRGrabbable>().isGrabbed;
+        SnapObject();
+    }
+}
