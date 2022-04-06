f7db2c14cc89993bc0c24925367935f343bd1a3f
ssambss <samppa.kemppainen@hotmail.com>
2022-04-06 20:40:31 +0300

    blood bag snap ja highlightit door handleihin

--- Blood Transfusion/Assets/TeleporttausScribu.cs
+++ Blood Transfusion/Assets/TeleporttausScribu.cs
@@ -8,7 +8,7 @@ public class TeleporttausScribu : MonoBehaviour
 {
     public bool isAtDestOne = false;
     [SerializeField] public GameObject player_rig;
-    [SerializeField] public GameObject destination, destination2, destination3;
+    [SerializeField] public GameObject destination, destination2, destination_recovery, destination3, destination4;
     
     void Start()
     {
@@ -26,9 +26,19 @@ public class TeleporttausScribu : MonoBehaviour
         player_rig.transform.position = destination2.transform.position;     
     }
 
+    public void Teleporttausrecovery()
+    {
+        player_rig.transform.position = destination_recovery.transform.position;
+    }
+
     public void Teleporttauskolme()
     {
         player_rig.transform.position = destination3.transform.position;
+    }
+
+    public void Teleporttausneljä()
+    {
+        player_rig.transform.position = destination4.transform.position;
     }
 
 }
